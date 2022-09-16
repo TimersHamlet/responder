99b9be34ff60943adf14999601aceea00e4f47d4
Lan <952694050@qq.com>
2022-09-16 23:44:57 +0800

    update:添加不同端的跳转功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'responder';
+export class AppComponent implements OnInit {
+  constructor(public platform: Platform,private router: Router){}
+  ngOnInit(): void {
+    if(this.platform.IOS||this.platform.ANDROID){
+      this.router.navigateByUrl('phone')
+    }else{
+      this.router.navigateByUrl('pc')
+    }
+  }
 }
