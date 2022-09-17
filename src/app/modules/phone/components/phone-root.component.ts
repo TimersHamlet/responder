94c75893fa3e0f9614311b67080696f80ad7201e
Lan <952694050@qq.com>
2022-09-17 12:38:07 +0800

    feat:完成tab

--- src/app/modules/phone/components/phone-root.component.ts
+++ src/app/modules/phone/components/phone-root.component.ts
@@ -3,21 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-phone-root',
   templateUrl: './phone-root.component.html',
-  styleUrls: ['./phone-root.component.scss']
+  styleUrls: ['./phone-root.component.scss'],
 })
 export class PhoneRootComponent {
-  tabList =[
+  tabList = [
     {
-      tabId:'main',
-      tabName:'主页',
-      iconUrl:'../../../../assets/icons/cat.png'
+      tabId: 'main',
+      tabName: '主页',
+      iconUrl: '../../../../assets/icons/cat.png',
     },
     {
-      tabId:'setting',
-      tabName:'设置',
-      iconUrl:'../../../../assets/icons/cat.png'
-    }
-  ] 
+      tabId: 'setting',
+      tabName: '设置',
+      iconUrl: '../../../../assets/icons/cat.png',
+    },
+  ];
 
-  activedUrl= '../../../../assets/icons/cat-actived.png'
+  activedUrl = '../../../../assets/icons/cat-actived.png';
+
+  currentID = '';
+
+  tabClick = (tabId:string) => {
+    this.currentID = tabId
+  };
 }
