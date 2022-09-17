141deb4c5ffdbb3195ce8b63b8f2a0eb9c87c488
Lan <952694050@qq.com>
2022-09-17 13:45:28 +0800

    feat:setting页面初始化

---
+++ src/app/modules/phone/components/setting/setting.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-phone',
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.scss'],
+})
+export class SettingPhoneComponent {
+}
--- src/app/modules/phone/phone-routing.module.ts
+++ src/app/modules/phone/phone-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPhoneComponent } from './components/main/main.component';
 import { PhoneRootComponent } from './components/phone-root.component';
+import { SettingPhoneComponent } from './components/setting/setting.component';
 
 const routes: Routes = [
   {
@@ -14,7 +15,7 @@ const routes: Routes = [
       },
       {
         path:'setting',
-        component:MainPhoneComponent
+        component:SettingPhoneComponent
       }
     ]
   }
